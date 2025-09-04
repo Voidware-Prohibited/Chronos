b088ac994ed9b7a66431f85c5733d3ab9e843232
colorindarkness <urielballinas@gmail.com>
2025-09-03 20:51:07 -0700

    Prototyping Types

---
+++ Source/Chronos/Public/Data/ChronosTypes.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include "ChronosTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct CHRONOS_API FChronosWeekDefinition
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (AllowPrivateAccess))
+	int Days;
+
+	bool operator==(const FChronosWeekDefinition& other) const
+	{
+		return (other.Days == Days);
+	}
+};
+
+USTRUCT(BlueprintType)
+struct CHRONOS_API FChronosMonthDefinition
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (AllowPrivateAccess))
+	FChronosWeekDefinition Weeks;
+
+	bool operator==(const FChronosMonthDefinition& other) const
+	{
+		return (other.Weeks == Weeks);
+	}
+};
+
+USTRUCT(BlueprintType)
+struct CHRONOS_API FChronosYearDefinition
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (AllowPrivateAccess))
+	TArray<FChronosMonthDefinition> Months;
+
+
+	bool operator==(const FChronosYearDefinition& other) const
+	{
+		return (other.Months == Months);
+	}
+};
+
+UCLASS(Blueprintable, BlueprintType)
+class CHRONOS_API UChronosYearDefinitionDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Features")
+	FChronosYearDefinition Years;
+};
